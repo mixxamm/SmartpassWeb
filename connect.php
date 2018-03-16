d725baa5b8088dc6b1f00a269afe280a54e1bd15
mixxamm <maximjanssensj5@gmail.com>
2018-03-16 11:15:42 +0100

    Verwijder connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'smartpass_one');
-   define('DB_PASSWORD', '7QHBEpgQRgWJvgkySXsWTxGf');
-   define('DB_DATABASE', 'smartpass_one');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
