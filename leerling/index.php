267887ab45f3afc6646a11b7aa0780f65bf8a47b
mixxamm <maximjanssensj5@gmail.com>
2018-05-05 18:49:55 +0200

    Maak werkend inlogsysteem voor leerlingen

---
+++ leerling/index.php
@@ -0,0 +1,33 @@
+<?php include ("login.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-light_blue.min.css" /> 
+<link rel="stylesheet" href="../css/style.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+</head>
+<body>
+<div class="container">
+<img id="logo" src="../images/Smartpass.png">
+				<h2>Leerling</h2>
+			<form id="login" action="" method="POST">
+			<div id="gebruikersnaam" class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input name="gebruikersnaam" class="mdl-textfield__input" type="text">
+			<label class="mdl-textfield__label">Gebruikersnaam</label>
+			</div>
+			<div id="wachtwoord" class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input name="wachtwoord" class="mdl-textfield__input" type="password">
+			<label class="mdl-textfield__label">Wachtwoord</label>
+			</div>
+			
+			<input id="loginknop" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" name="submit" value="Log In">
+			</form>
+			
+			<div><?php echo $error; ?></div>
+			</br>
+			<a href="#"><p class="small">Wachtwoord vergeten?</p></a>
+		</div>
+</body>
+</html>
---
+++ leerling/login.php
@@ -0,0 +1,25 @@
+<?php
+   include("../connect.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+     
+      
+      $user_name = mysqli_real_escape_string($db,$_POST['gebruikersnaam']);
+      $user_pass = mysqli_real_escape_string($db,$_POST['wachtwoord']); 
+      
+      $mysql_query = "SELECT * FROM tblleerlingen WHERE Naam like '$user_name';";
+      $result = mysqli_query($db,$mysql_query);
+      $row = mysqli_fetch_assoc($result);
+      $hash = $row['Wachtwoord'];
+		
+      if(password_verify($user_pass, $hash)) {
+         $_SESSION['login_user'] = $user_name;
+         $_SESSION['pass_user'] = $user_pass;
+         
+         header("location: kaart.php");
+      }else {
+         $error = "Uw gebruikersnaam of wachtwoord is fout.";
+      }
+   }
+?>
