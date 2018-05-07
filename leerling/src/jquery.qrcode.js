9d588917be5f07c1db7968a33013343edaa3ab6e
mixxamm <maximjanssensj5@gmail.com>
2018-05-07 15:37:39 +0200

    Maak nieuwe layout en verander achtergrondkleur
    Jorn heeft de nieuwe layout gemaakt, deze moet nog wel worden verbeterd.

--- leerling/src/jquery.qrcode.js
+++ leerling/src/jquery.qrcode.js
@@ -1,76 +1,4 @@
-
-<?php
-include('../connect.php');
-session_start();
-$id = $_SESSION['leerlingid'];
-$foto = 'https://smartpass.one/foto/'.$id.'.png';
-$mysql_qry = "select * from tblleerlingen where LeerlingID like $id;";
-$result = mysqli_query($db, $mysql_qry);
-if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
-    $naam = $row['Naam'];
-    $klasnr = $row['Klas'];
-    $Buiten = $row["Buiten"];
-    $klas_qry = "select Klas from tblklassen where KlasNr like $klasnr;";
-    $resultklas = mysqli_query($db,$klas_qry);
-    
-    if($Buiten == "1"){
-        $mysql_qry1 = "select * from tbltelaat where leerlingID like '$id';";
-        $result1 = mysqli_query($db, $mysql_qry1);
-        $row1 = mysqli_fetch_assoc($result1);
-        $datum = date("Y-m-d");
-            $datumnaarbuiten = $row1["Datum_naar_buiten"];
-            if($datumnaarbuiten > $datum){
-                $Buiten = "0";
-            }
-            else{
-                $Buiten = "1";
-            }
-    }
-    
-    if($Buiten == "1"){
-        $kleur = "#8BC34A";
-    }
-    else{
-        $kleur = "#F44336";
-    }
-    
-    if (mysqli_num_rows($resultklas) > 0) {
-        $rowklas = mysqli_fetch_assoc($resultklas);
-        $klas = $rowklas['Klas'];
-    }
-}
-?>
-<html>
-<head>
-    <meta name="theme-color" content="<?php echo $kleur ?>" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="<?php echo $kleur ?>">
-<title>Smartpass</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-
-<script>
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('klok').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // voeg 0 toe voor enkele cijfers
-    return i;
-}
-</script>
-</head>
-<body onload="startTime()" style="background-color: <?php echo $kleur ?>">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-<div id="klok"></div>
-<!--<script type="text/javascript" src="../jquery.qrcode.min.js"></script>
---><script>(function( $ ){
+(function( $ ){
 	$.fn.qrcode = function(options) {
 		// if options is string, 
 		if( typeof options === 'string' ){
@@ -85,7 +13,7 @@ function checkTime(i) {
 			height		: 256,
 			typeNumber	: -1,
 			correctLevel	: QRErrorCorrectLevel.H,
-                        background      : "<?php echo $kleur ?>",
+                        background      : "#ffffff",
                         foreground      : "#000000"
 		}, options);
 
@@ -159,20 +87,3 @@ function checkTime(i) {
 		});
 	};
 })( jQuery );
-</script>
-<script type = "text/javascript" src = "src/qrcode.js"></script>
-<table class = "leerlingenkaarttabel" style="background-color: <?php echo $kleur ?>">
-    <tr><img width="600px" height="600px" src = '<?php echo $foto ?>'></tr>
-    <tr>
-        <p><?php echo $naam ?></p>
-    <p><?php echo $klas ?></p></tr>
-    <tr><div id="qrcode"></div></tr>
-  </table>
-  
-<script>
-	jQuery('#qrcode').qrcode({width: 600,height: 600,text: '<?php echo $id ?>'});
-</script>  
-
-
-</body>
-</html>
